e821a14f558ea19ca5e7022b5ce11842d3683d17
Spoorthikm57 <spoorthikm2k7@gmail.com>
2026-04-11 20:22:45 +0530

    Fresh upload

---
+++ brainwave-recall/src/main/java/com/example/brainwave_recall/BrainwaveRecallApplication.java
@@ -0,0 +1,13 @@
+package com.example.brainwave_recall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainwaveRecallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainwaveRecallApplication.class, args);
+	}
+
+}
---
+++ brainwave-recall/src/test/java/com/example/brainwave_recall/BrainwaveRecallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brainwave_recall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainwaveRecallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
